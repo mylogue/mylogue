6a13101efe17faf5aee9021ea3fe08cae2b0ad7c
mylogue.proj <mylogue.proj@gmail.com>
2024-02-20 12:10:33 +0900

    merge

--- src/components/auth-components.ts
+++ src/components/auth-components.ts
@@ -146,11 +146,7 @@ export const Submit = styled.input`
     cursor: pointer;
     
     &:hover{
-<<<<<<< HEAD
       color: #fff;
-=======
-      opacity: 0.8;
->>>>>>> develop
     }
 `;
 
@@ -165,12 +161,6 @@ export const Input = styled.input`
   &::placeholder{
     color: var(--gray400, #BDC5CC);
   }
-<<<<<<< HEAD
-=======
-  &:focus{
-    color: var(--gray600); 
-  }
->>>>>>> develop
 `;
 
 
@@ -181,7 +171,6 @@ export const Error = styled.span`
 
 export const Switcher = styled.span`
   margin-top: 20px;
-<<<<<<< HEAD
   color: #95A1AC;
   font-size: 14px;
   
@@ -193,9 +182,5 @@ export const Switcher = styled.span`
     color: #0085FF;
     text-decoration: none;
     margin-left: 4px;
-=======
-  a {
-    color: #1d9bf0;
->>>>>>> develop
   }
 `;
