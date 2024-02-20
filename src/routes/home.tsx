5ccfda1942e14012fc2a8a9ce461c1fcf593fed7
yvonne lim <yvonne.jw.lim@gmail.com>
2024-02-20 12:12:36 +0900

    post-form style

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,12 @@
 import { styled } from "styled-components";
-import PostTweetForm from "../components/post-form";
+import PostForm from "../components/post-form";
 
 const Wrapper = styled.div``;
 
 export default function Home() {
   return (
     <Wrapper>
-      <PostTweetForm />
+      <PostForm />
     </Wrapper>
   );
 }
