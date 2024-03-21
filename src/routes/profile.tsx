4bb199a67ccd987fad93bb58bd3b73e6becb0fe8
mylogue.proj <mylogue.proj@gmail.com>
2024-03-21 21:23:10 +0900

    Feat: 프로필 편집 + 유저 타임라인

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -3,7 +3,7 @@ import { auth, db, storage } from "../firebase";
 import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 import { ITweet } from "../components/timeline";
 import Tweet from "../components/tweet";
 export interface ITweet {
@@ -67,6 +67,10 @@ const ProfileInfo = styled.div`
             font-style: normal;
             font-weight: 700;
             line-height: normal;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            gap: 10px;
         }
         span:last-child{
             color: var(--gray600, #606E7B);
@@ -125,44 +129,87 @@ const Name = styled.span`
   font-size: 22px;
 `;
 
+const CommonBox = styled.article`
+    background: #fff;
+    border-radius: 8px;
+    margin: 0 32px;
+`;
+const DisplaynameInput = styled.input``;
+const EditImg = styled.div`
+  width: 24px;
+  height: 24px;
+  color: #1d9bf0;
+  cursor: pointer;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    svg {
+      opacity: 0.8;
+    }
+  }
+`;
+
 export default function Profile(){
     const user = auth.currentUser;
     const [avatar, setAvatar] = useState(user?.photoURL);
-    const onAvatarChange = async (e:React.ChangeEvent<HTMLInputElement>) => {
-        const {files} = e.target;
+    const [tweets, setTweets] = useState<ITweet[]>([]);
+    const [displayname, setDisplayname] = useState(user?.displayName ?? "");
+    const [editDisplayname, setEditDisplayname] = useState(false);
+    const onEditChange = async () => {
         if(!user) return;
-        if(files && files.length === 1){
-            const file = files[0];
-            const locationRef = ref(storage, `avatars/${user?.uid}`);
-            const result = await uploadBytes(locationRef, file);
-            const avatarUrl = await getDownloadURL(result.ref);
-            setAvatar(avatarUrl);
-            await updateProfile(user, {photoURL:avatarUrl,});
+        if(!editDisplayname){
+            setEditDisplayname(true);
+        } else{
+            await updateProfile(user,{
+                displayName:displayname,
+            })
+            setEditDisplayname(false);
         }
     }
-    const [tweets, setTweet] = useState<ITweet[]>([]);
+    const onDisplaynameChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setDisplayname(e.target.value);
+    }
+    const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = e.target;
+        if (!user) return;
+        if (files && files.length === 1) {
+        const file = files[0];
+        const locationRef = ref(storage, `avatars/${user?.uid}`);
+        const result = await uploadBytes(locationRef, file);
+        const avatarUrl = await getDownloadURL(result.ref);
+        setAvatar(avatarUrl);
+        await updateProfile(user, {
+            photoURL: avatarUrl,
+        });
+        }
+    };
     const fetchTweets = async () => {
-      const tweetsQuery = query(
+        const tweetQuery = query(
         collection(db, "tweets"),
-        orderBy("createdAt", "desc")
-      );
-      const spanshot = await getDocs(tweetsQuery);
-      const tweets = spanshot.docs.map((doc) => {
+        where("userId", "==", user?.uid),
+        orderBy("createdAt", "desc"),
+        limit(25)
+        );
+        const snapshot = await getDocs(tweetQuery);
+        const tweets = snapshot.docs.map((doc) => {
         const { tweet, createdAt, userId, username, photo } = doc.data();
         return {
-          tweet,
-          createdAt,
-          userId,
-          username,
-          photo,
-          id: doc.id,
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
         };
-      });
-      setTweet(tweets);
+        });
+        setTweets(tweets);
     };
     useEffect(() => {
-      fetchTweets();
+        fetchTweets();
     }, []);
+    
     return (
         <div>
             <ProfileBg>
@@ -177,20 +224,49 @@ export default function Profile(){
             </ProfileBg>
             <AvatarInput
                 onChange={onAvatarChange}
-                id="avatarBg"
-                type="file"
-                accept="image/*"
-            />
-            <AvatarInput
-                onChange={onAvatarChange}
                 id="avatar"
                 type="file"
                 accept="image/*"
             />
             <ProfileInfo>
                 <p>
-                    <Name>{user?.displayName ?? "Anonymous"}</Name>
-                    <span>@{user?.displayName ?? "Anonymous"}</span>
+                    <Name>
+                        {editDisplayname ? (
+                             <DisplaynameInput
+                             onChange={onDisplaynameChange}
+                             placeholder={displayname}
+                           />
+                        ) : (
+                            user?.displayName ?? "Anonymous"
+                        )}
+                        <EditImg onClick={onEditChange}>
+                            {editDisplayname ? (
+                                <svg
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                                >
+                                <path
+                                    clipRule="evenodd"
+                                    fillRule="evenodd"
+                                    d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                                />
+                                </svg>
+                            ) : (
+                                <svg
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                                >
+                                <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                                </svg>
+                            )}
+                        </EditImg>
+                    </Name>
+                    <span>@{user?.uid ?? "Anonymous"}</span>
                 </p>
                 <p className="comment">솰라솰라 자기소개 한마디 욜로로</p>
                 <p>
@@ -198,9 +274,13 @@ export default function Profile(){
                     <span className="followers">101</span>
                 </p>
             </ProfileInfo>
-            {tweets.map((tweet) => (
-                <Tweet key={tweet.id} {...tweet} />
-            ))}
+            
+            <CommonBox>
+                {tweets.map((tweet) => (
+                    <Tweet key={tweet.id} {...tweet} />
+                ))}
+                
+            </CommonBox>
         </div>
     );
 }
