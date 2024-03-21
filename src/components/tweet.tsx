8d6ff8ccf388aa15e0e577b4f5322d0f65aea105
yvonne lim <yvonne.jw.lim@gmail.com>
2024-03-21 21:25:45 +0900

    delete Tweet

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,9 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+
 const Wrapper = styled.div`
   display: grid;
   padding: 16px;
@@ -21,23 +25,42 @@ const UserPic = styled.div`
     height: 60px;
     border-radius: 100px;
     background: #0085FF;
+    cursor: pointer;
+`;
+
+const DeleteBtn = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    svg {
+      width: 18px;
+      color: #606E7B;
+      margin-right: 15px;
+      cursor: pointer;
+  }
 `;
 
 const Photo = styled.img`
   width: 100px;
   height: 100px;
   border-radius: 15px;
+  cursor: pointer;
 `;
+
 const Username = styled.span`
   font-weight: 800;
   font-size: 20px;
+  cursor: pointer;
 `;
+
 const UserId = styled.span`
   font-weight: 600;
   font-size: 16px;
   color : #606E7B;
   margin-left: 8px;
+  cursor: pointer;
 `;
+
 const Payload = styled.p`
   margin: 10px 0px;
   font-size: 16px;
@@ -59,6 +82,7 @@ const LeftIcon = styled.div`
         color: #0F172A;
         float: left;
         margin-right: 15px;
+        cursor: pointer;
     }
 `;
 
@@ -70,18 +94,42 @@ const RightIcon = styled.div`
         width: 24px;  
         color: #0F172A;
         margin-right: 15px;
+        cursor: pointer;
     }
 `;
 
 
 
 export default function Tweet({ userId, username, photo, tweet }: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("Are you sure you want to delete this tweet?");
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      //
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <UserPic></UserPic>
         <Username>{username}</Username>
         <UserId>@{userId}</UserId>
+        {user?.uid === userId ? (
+        <DeleteBtn onClick={onDelete}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+          </svg>
+        </DeleteBtn>
+        ) : null}
         <Payload>{tweet} {photo ? <Photo src={photo} /> : null} </Payload>
         <TextBottom>
           <LeftIcon>
