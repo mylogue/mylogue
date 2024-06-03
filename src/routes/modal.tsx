e86d4bece526f53febed57b6f1699fd510dab2a2
yvonne lim <yvonne.jw.lim@gmail.com>
2024-06-04 00:19:49 +0900

    redesign

--- src/routes/modal.tsx
+++ src/routes/modal.tsx
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 import PostForm from "../components/post-form";
 
+
 const ModalWrap = styled.div`
   width: 100vw;
   height: 100vh;
@@ -20,12 +21,28 @@ const ModalContainer = styled.div`
   top: 20%;
   left: 20%;
   width: 60%;
-  padding: 3rem 2rem;
+  padding: 1rem 2rem 1.85rem 2rem;
   background: #E2E6EA;
   border-radius: 10px;
 `;
 
+const StyledPostForm =styled(PostForm)`
+    height: 15rem;
+    width: 100%;
+    color: #384048;
+    background-color: white;
+`;
+
 const CloseBtn = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    svg {
+    width: 1.5rem;
+    color: #606E7B;
+    margin: -.4rem -1.5rem 0 0; 
+    cursor: pointer;
+    }
 `;
 
 export default function () {
@@ -34,8 +51,10 @@ export default function () {
     <ModalWrap>
         <ModalBackGround/>
         <ModalContainer>
-            <CloseBtn/>
-            <PostForm/>
+            <CloseBtn>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path></svg>
+            </CloseBtn>
+            <StyledPostForm/>
         </ModalContainer>
     </ModalWrap>
   );
