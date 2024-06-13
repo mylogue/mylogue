07dbd7406bb6d49fc9c1118d6cca311c6a9e397f
yvonne lim <yvonne.jw.lim@gmail.com>
2024-06-13 20:00:57 +0900

    modal-fixing:

--- src/routes/modal.tsx
+++ src/routes/modal.tsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 import PostForm from "../components/post-form";
+import { useHistory } from "react-router-dom";
+import { ITweet} from "../components/timeline";
 
 
 const ModalWrap = styled.div`
@@ -51,9 +53,9 @@ export default function () {
     <ModalWrap>
         <ModalBackGround/>
         <ModalContainer>
-            <CloseBtn>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path></svg>
-            </CloseBtn>
+                <CloseBtn>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path></svg>
+                </CloseBtn>
             <StyledPostForm/>
         </ModalContainer>
     </ModalWrap>
