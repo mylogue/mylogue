f87b17f88523d20159f7cbb623bad395a16c80ae
yvonne lim <yvonne.jw.lim@gmail.com>
2024-06-14 20:32:58 +0900

    back closing

--- src/routes/modal.tsx
+++ src/routes/modal.tsx
@@ -54,7 +54,7 @@ export default function() {
 
   return (
     <ModalWrap>
-        <ModalBackGround/>
+        <ModalBackGround onClick={closeModal} />
         <ModalContainer>
                 <CloseBtn onClick={closeModal}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path></svg>
